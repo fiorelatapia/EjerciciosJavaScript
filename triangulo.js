4b51fb4758255ead1dd246df97830f35fec60683
Fiorela Tapia Cárdenas <165432821+fiorelatapia@users.noreply.github.com>
2024-04-20 07:57:19 +0000

    Update triangulo.js

---
+++ triangulo.js
@@ -0,0 +1,4 @@
+let base=parseFloat(prompt("Ingrese el valor de la base: "));
+let altura=parseFloat(prompt("Ingrese el valor de la altura: "));
+let calculo= (base*altura)/2;
+console.log(`El área del triángulo es ${calculo}`)
